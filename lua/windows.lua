0c4601e95cef29026c930760d8a53f29d0ab74d9
Leno <leno.development@outlook.com>
2026-04-14 20:53:54 +0200

    tst

--- lua/windows.lua
+++ lua/windows.lua
@@ -63,8 +63,9 @@ end
 local function open_file_in_window_buffer(window,buffer,path)
     print(".. " .. window .. "  " .. buffer)
     vim.api.nvim_win_set_buf(window,buffer)
-    vim.api.nvim_set_current_win(window)
+    vim.api.nvim_set_current_win(window):
     vim.api.nvim_buf_set_name(buffer, path)
+    vim.fn.bufload(buffer)
 end
 
 
