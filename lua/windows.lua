a6f2af1354b3fd2479fca61f5d3cdb1ae1628c9f
Leno <leno.development@outlook.com>
2026-04-15 19:35:51 +0200

    Test

--- lua/windows.lua
+++ lua/windows.lua
@@ -110,7 +110,7 @@ end
 local function display_file_index_buffer_in_center_view_virtual_text()
     vim.api.nvim_buf_set_extmark(0, my_ns, 0, 0, {
         virt_lines = {{"📝 REMARK: This buffer is read-only", "WarningMsg"}},
-        virt_lines_above = true,
+        virt_text_pos = "eol",  -- Position at end of line
     })
 end
 
