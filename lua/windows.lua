ded79d614d321772bd9ba1cc8a664e466144356f
Leno <leno.development@outlook.com>
2026-04-16 08:57:32 +0200

    Test

--- lua/windows.lua
+++ lua/windows.lua
@@ -440,15 +440,16 @@ end
 local function on_close_event_check_for_center_window(win_id)
     if win_id == M.windows.center_window.win_id then
 		print("Hello my dear friend")
-          vim.api.nvim_set_current_win(M.windows.left_window.win_id)
-            local center_win = vim.api.nvim_open_win(M.windows.center_window.buffers[M.windows.center_window.current_buffer_index].id, true, {
-                split = "right",  -- Opens to the right
-                vertical = true,   -- Vertical split
-            })
-            M.windows.center_window.win_id = center_win
-          
-            M.navigator.current_parent_win = center_win
-            return true
+        local config_center = vim.api.nvim_win_get_config(win_id)
+        vim.api.nvim_set_current_win(M.windows.left_window.win_id)
+        local center_win = vim.api.nvim_open_win(M.windows.center_window.buffers[M.windows.center_window.current_buffer_index].id, true, {
+            split = "right",  -- Opens to the right
+            vertical = true,   -- Vertical split
+        })
+        M.windows.center_window.win_id = center_win
+        M.navigator.current_parent_win = center_win
+        vim.api.nvim_win_set_config(M.windows.center_window.win_id, config_center)
+        return true
     end
 end
 
