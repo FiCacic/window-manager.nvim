664952db49a7259a733e6aed67019e9941d9e3c1
Leno <leno.development@outlook.com>
2026-04-16 09:04:44 +0200

    Test

--- lua/windows.lua
+++ lua/windows.lua
@@ -420,7 +420,7 @@ local function counter_resizing_of_windows(initial)
         config_left.width = M.windows.left_window.style.width
         config_left.height = M.windows.left_window.style.height
 
-        config_center.width = M.windows.center_window.style.width+10
+        config_center.width = M.windows.center_window.style.width+100
         config_center.height = M.windows.center_window.style.height
 
         config_right.width = M.windows.right_window.style.width
